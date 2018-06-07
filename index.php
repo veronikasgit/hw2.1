b550bc38b5ec90f30c160d60c304b6fd76f004a4
Veronika <w_e_r_o_n_i_k_a@mail.ru>
2018-06-07 23:50:03 +0300

    final

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-$json = file_get_contents(__DIR__ . '/content2.json');
+$json = file_get_contents(__DIR__ . '/content.json');
 $data = json_decode($json, true);
 
-var_dump($data); 
 ?>
 
 <html>
@@ -13,38 +12,25 @@ var_dump($data);
 <body>
 	<table>
 
-		<tread>
 			<tr>
 				<td>Имя</td>
 				<td>Фамилия</td>
 				<td>Адрес</td>
 				<td>Телефон</td>
 			</tr>
-		</tread>
-
-		<tbody>
+		
 		<?php foreach($data as $item) { ?>
 			<tr>
 				<td><?php echo $item['firstName']; ?></td>
-				<td><?php echo $item['secondName']; ?></td>
-				<td><?php foreach($item as $key=>$value) { ?>
-					<ul>
-						<li><?php echo $value['city']; ?></li>
-						<li><?php echo $value['street']; ?></li>
-						<li><?php echo $value['home']; ?></li>
-					</ul>
-					<?php } ?>
-				</td>
-				
+				<td><?php echo $item['lastName']; ?></td>
+				<td><?php echo $item['address']; ?></td>
+				<td><?php echo $item['phoneNumber']; ?></td>
 			</tr>
 		<?php } ?>
-		</tbody>
 	</table>
 
 </body>
 </html>
-
-
 
 
 
